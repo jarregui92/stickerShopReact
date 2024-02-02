9ca2106186cfeaf39e6ccbcfd57e261a729e3964
jarregui92 <jarregui92@gmail.com>
2024-02-01 22:02:36 -0300

    Se arreglo un poco el estilo de las tarjetas

--- src/componentes/Item/Item.jsx
+++ src/componentes/Item/Item.jsx
@@ -2,37 +2,32 @@ import './Item.css';
 import { Link } from 'react-router-dom';
 import Rating from '../Rating/Rating';
 
-const Item = ({id, nombre, precio, img, rate, moneda}) => {
+const Item = ({ id, nombre, precio, img, rate, moneda }) => {
   return (
-    <>
-            <div className="relative w-full h-64">
-              <img src={img} alt={nombre} sizes="100%"
-                  style={{
-                      //ACOMODAR ESTO PARA QUE SE VEAN MEJOR LAS IMAGENES
-                      objectFit: 'contain',
-                  }}
-              />
-            </div>
-            <div className="p-6" >
-            <p className="font-semibold text-lg" >{nombre}</p>
-            <Rating rate={rate}/>
-            <div className="mt-4 flex items-center justify-between space-x-2">
-                <div>
-                    <p className="text-gray-500" >Price</p>
-                    <p className="text-lg font-semibold" ><span >{moneda}</span>{precio}</p>
-                </div>
-                <Link to={`/item/${id}`}>
-                  <button className="border rounded-lg py-1 px-4 hover:bg-green-500 hover:text-white">
-                      Detalles
-                  </button>
-                </Link>
-            </div>
+    <div className='border-2 rounded-md group overflow-hidden bg-gray-100'>
+      <div className="relative w-full bg-gray-400">
+        <img src={img} alt={nombre} className="object-cover" />
+      </div>
+      <div className="p-6" >
+        <p className="font-semibold text-lg" >{nombre}</p>
+        <Rating rate={rate} />
+        <div className="mt-4 flex items-center justify-between space-x-2">
+          <div>
+            <p className="text-gray-500" >Price</p>
+            <p className="text-lg font-semibold" ><span >{moneda}</span>{precio}</p>
+          </div>
+          <Link to={`/item/${id}`}>
+            <button className="border rounded-lg py-1 px-4 hover:bg-green-500 hover:text-white">
+              Detalles
+            </button>
+          </Link>
         </div>
+      </div>
 
-        
-    </>
-    
-    
+
+    </div>
+
+
   )
 }
 
--- src/componentes/ItemList/ItemList.jsx
+++ src/componentes/ItemList/ItemList.jsx
@@ -1,10 +1,12 @@
 import Item from "../Item/Item";
-import './ItemList.css';
+//import './ItemList.css';
 
-const ItemList = ({productos}) => {
+const ItemList = ({ productos }) => {
   return (
-    <div className="contenedorProductos">
+    <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
+      <div className="grid gap-8 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1">
         {productos.map(prod => <Item key={prod.id}  {...prod} />)}
+      </div>
     </div>
   )
 }
