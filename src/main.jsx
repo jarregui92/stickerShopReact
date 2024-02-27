49af63070c5de4b0779ac21847fe70cc701e3886
jarregui92 <jarregui92@gmail.com>
2024-02-27 17:20:36 -0300

    Se descomento React.StrictMode para ver si asi se soluciona el error en vercel al actualizar la pagina

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './main.css'
+import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
+  <React.StrictMode>
     <App />
-  //</React.StrictMode>,
+  </React.StrictMode>,
 )
